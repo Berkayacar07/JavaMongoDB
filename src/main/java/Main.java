c25857d2cd90b085ce148491b757abe9a25d431f
Berkayacar07 <berkay-acrr@hotmail.com>
2022-07-14 16:52:22 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.client.*;
 import org.bson.Document;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,7 +12,19 @@ public class Main {
                 ("mongodb+srv://berkayacar07:190307@cluster0.ckosjqr.mongodb.net/?retryWrites=true&w=majority");
         MongoDatabase db = client.getDatabase("sampleDB");
         MongoCollection col = db.getCollection("sampleCollection");
-        Document sampleDoc = new Document("_id","1").append("name","John Smith");
-        col.deleteOne(sampleDoc);
+
+//        MongoCursor<String> dbsCursor = client.listDatabaseNames().iterator();
+//        while (dbsCursor.hasNext()){
+//            System.out.println(dbsCursor.next());
+//        }
+        List<Integer> books = Arrays.asList(27464, 747854);
+        Document person = new Document("_id", "2")
+                .append("name","Berkay" )
+                .append("address", new BasicDBObject("street", "Ulusoy St.")
+                        .append("city", "Antalya")
+                        .append("state", "MA")
+                        .append("zip", 12345))
+                .append("books", books);
+        col.insertOne(person);
     }
 }
